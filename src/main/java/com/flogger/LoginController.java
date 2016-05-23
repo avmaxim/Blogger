5ccf8714c5c6b8e32787032415ac5d15757627de
avmaxim <19andrewking96@gmail.com>
2016-05-23 09:16:30 -0700

    Develop & Fill in MySQL database 'booksdb'. Add DBOutputController servlet.

--- src/main/java/com/flogger/LoginController.java
+++ src/main/java/com/flogger/LoginController.java
@@ -7,11 +7,14 @@ package com.flogger;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.naming.*;
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import javax.sql.DataSource;
 
 @WebServlet(
         urlPatterns = {"/login"},
@@ -20,21 +23,33 @@ import javax.servlet.http.HttpServletResponse;
                 @WebInitParam(name = "username", value = "avmaxim"),
                 @WebInitParam(name = "password", value = "hello")
         })
+
 public class LoginController extends HttpServlet implements Servlet{
+
+    DataSource pool;    //Database Connection Pool
 
     public LoginController(){
         super();
     }
 
+    @Override
     public void init() throws ServletException {
-        //we can create DB connection resource here and set it to Servlet context
-        if(getServletContext().getInitParameter("db_url").equals("jdbc:mysql://localhost/mysql_db") &&
-                getServletContext().getInitParameter("db_username").equals("mysql_username") &&
-                getServletContext().getInitParameter("db_password").equals("mysql_password"))
+        initDatabaseConnection("booksdb");
+    }
 
+    private void initDatabaseConnection(String databaseName) throws ServletException{
+        try {
+            // Create a JNDI Initial context to be able to lookup the DataSource
+            InitialContext ctx = new InitialContext();
+            // Lookup the DataSource, which will be backed by a pool
+            //   that the application server provides.
+            pool = (DataSource)ctx.lookup("java:comp/env/jdbc/" + databaseName);
+            if (pool == null)
+                throw new ServletException("Unknown DataSource 'jdbc/" + databaseName + "'");
             getServletContext().setAttribute("db_success", "true");
-        else
-            throw new ServletException("Database Connection error");
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        }
     }
 
     @Override
@@ -44,20 +59,51 @@ public class LoginController extends HttpServlet implements Servlet{
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        //get servlet config init params
-        String usernameDB = getServletConfig().getInitParameter("username");
-        String passwordDB = getServletConfig().getInitParameter("password");
+        // Allocate an output writer to write the response message into the network socket
+        PrintWriter out = response.getWriter();
 
-        //logging example
-        log("Username="+username+"::password="+password);
+        Connection conn = null;
+        Statement statement = null;
 
-        if(usernameDB.equals(username) && passwordDB.equals(password)){
-            response.sendRedirect("/views/auth-success.jsp");
-        }else{
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/views/login.jsp");
-            PrintWriter out= response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(request, response);
+        try{
+             // Get a connection from the pool
+            conn = pool.getConnection();
+            statement = conn.createStatement();
+
+            ResultSet records = statement.executeQuery("SELECT Username, Password FROM user");
+
+            String usernameDB = null;
+            String passwordDB = null;
+            Boolean isExists = false;
+
+            while(records.next()){
+                usernameDB = records.getString("Username");
+                passwordDB = records.getString("Password");
+
+                if(usernameDB.equals(username) && passwordDB.equals(password)) {
+                    isExists = true;
+                    response.setContentType("text/html;charset=UTF-8");
+                    response.sendRedirect("/views/auth-success.jsp");
+                    break;
+                }
+            }
+
+            if(!isExists) {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/views/login.jsp");
+                out.println("<font color=red>Either user name or password is wrong.</font>");
+                rd.include(request, response);
+            }
+
+        }catch(SQLException ex) {
+            ex.printStackTrace();
+        }finally{
+            out.close();
+            try {
+                if (statement != null) statement.close();
+                if (conn != null) conn.close();  // return to pool
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
--- src/main/webapp/views/auth-success.jsp
+++ src/main/webapp/views/auth-success.jsp
@@ -7,7 +7,7 @@
 <body>
     <div style="text-align:center; margin-top: 100px;">
         <h1> Suuuuuuuccessful! Welcome to my world!</h1>
-        
+        <h1> Go to <a href="/database-contents">Database Contents</a></h1>
     </div>
 </body>
 </html>
