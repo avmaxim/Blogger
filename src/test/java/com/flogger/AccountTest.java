29991b968dbea26dcac15126d790fa57268752bf
Dima Gurin <gurizavr1994@gmail.com>
2016-05-30 13:11:22 +0300

    Add .gitignore to .idea folder. Fix bug AccountTest. Add new tests.

--- src/test/java/com/flogger/AccountTest.java
+++ src/test/java/com/flogger/AccountTest.java
@@ -2,6 +2,7 @@ package com.flogger;
 
 import com.flogger.controllers.AccountController;
 import com.flogger.models.UserLoginModel;
+import com.flogger.models.UserRegistrationModel;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -27,25 +28,33 @@ public class AccountTest {
     public void NullUserTest(){
         UserLoginModel model = new UserLoginModel();
         model.setUsername("somename");
-        model.setPassword("somepassword");/*
+        model.setPassword("somepassword");
         FloggerJsonData<UserLoginModel> result = FloggerJsonData.GetAsJson(model);
+        model.setPassword("somepassword");
         result = myAccountController.loginUser(model);
         if (result == null)
-            Assert.fail();*/
+            Assert.fail();
+    }
+
+
+    @Test
+    public void logoutTest(){
+        FloggerJsonData fjd = new FloggerJsonData();
+        fjd = myAccountController.logoutUser();
+        if (fjd == null)
+            Assert.fail();
     }
 
     @Test
-    public void EmptyUserTest(){
-        UserLoginModel model = new UserLoginModel();
-        model.setUsername("df");
-        model.setUsername("sfsfsfsfseee");/*
-        UserLoginModel result = new UserLoginModel();
-        result = myAccountController.loginUser(model);
-        String name = result.getUsername();
-        String password = result.getPassword();
-        if ((name == "") || (password == ""))
-            Assert.fail();*/
-
+    public void RegistrUserTest(){
+        FloggerJsonData fjd;
+        UserRegistrationModel model = new UserRegistrationModel();
+        model.setUsername("sfkww");
+        model.setPassword("kelse");
+        model.setEmail("cucu@gmail.com");
+        fjd = myAccountController.registerUser(model);
+        if (fjd == null)
+            Assert.fail();
     }
 
 
