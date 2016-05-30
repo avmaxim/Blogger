03181d4eff47e892ca8d2d3f274e12250d34d8d5
Dima Gurin <gurizavr1994@gmail.com>
2016-05-30 13:11:22 +0300

    Add .gitignore to .idea folder. Fix bug AccountTest. Add new tests.

--- src/test/java/com/flogger/DomainsTests.java
+++ src/test/java/com/flogger/DomainsTests.java
@@ -1,5 +1,8 @@
 package com.flogger;
 import com.flogger.controllers.AccountController;
+import com.flogger.domains.Article;
+import com.flogger.domains.Avertisement;
+import com.flogger.domains.User;
 import com.flogger.models.UserLoginModel;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,4 +19,64 @@ import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DomainsTests {
+
+    @Test
+    public void ArticleLikesCountTest() {
+        Article ar = new Article();
+        int likeCount = 10;
+        ar.setLikesCount(likeCount);
+        if (ar.getLikesCount() < 0)
+            Assert.fail();
+    }
+
+    @Test
+    public void ArticleOwnerTest(){
+        Article ar = new Article();
+        ar.setOwner(new User());
+        if (ar.getOwner() == null)
+            Assert.fail();
+    }
+
+    @Test
+    public void ArticleAttributesTest(){
+        Article ar = new Article();
+        ar.setContents("Food");
+        ar.setHeader("Potato");
+        ar.setTimestamp(new Date(72652528));
+        if (ar.getContents() == "" || ar.getHeader() == "" || ar.getTimestamp() == null){
+            Assert.fail();
+        }
+    }
+
+    @Test
+    public void AdvertisementContentTest(){
+        Avertisement avd = new Avertisement();
+        avd.setContents("try twix");
+        if (avd.getContents() == "")
+            Assert.fail();
+    }
+
+    @Test
+    public void AdvertisementHeaderTest(){
+        Avertisement adv = new Avertisement();
+        adv.setHeader("Yoooo!!!");
+        if (adv.getHeader() == "")
+            Assert.fail();
+    }
+
+    @Test
+    public void AdvertisementIdTest(){
+        Avertisement adv = new Avertisement();
+        adv.setId(23);
+        if (adv.getId() < 0)
+            Assert.fail();
+    }
+
+    @Test
+    public void AdvertisementArticleTest(){
+        Avertisement adv = new Avertisement();
+        adv.setArticle(new Article());
+        if (adv.getArticle() == null)
+            Assert.fail();
+    }
 }
